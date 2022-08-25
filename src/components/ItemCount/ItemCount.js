520e34e016a168e5918a546b945250064cda00e4
Alberto Garza Ayala <alberto.garzaay@outlook.com>
2022-08-25 00:02:55 -0500

    Avances del desafio 5

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -22,19 +22,19 @@ export const ItemCount = () => {
     return(
         <div className="container my-2">
             <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={process.env.PUBLIC_URL + './img/globo_graduacion.jpeg'}/>
-      <Card.Body className='primerCard'>
-        <Card.Title>Globo de Graduacion</Card.Title>
-        <Card.Text>
-          Tu ser querido termina su ciclo de estudios? Felicitalo con un hermoso globo para que nunca olvide
-          este increible momento
-        </Card.Text>
-        <button onClick={handlerestar} className='btn btn-outline-dark '>-</button>
-            <span className='mx-2'> {counter} </span>
-        <button onClick={handlesumar} className='btn btn-dark '>+</button>
-        <Button variant="dark m-2 start-50">Agregar al Carrito</Button>
-      </Card.Body>
-    </Card>
+              <Card.Img variant="top" src={process.env.PUBLIC_URL + './img/burbujas/globo_graduacion.jpeg'}/>
+                <Card.Body className='primerCard'>
+                   <Card.Title>Globo de Graduacion</Card.Title>
+                      <Card.Text>
+                          Tu ser querido termina su ciclo de estudios? Felicitalo con un hermoso globo para que nunca olvide
+                          este increible momento
+                      </Card.Text>
+                          <button onClick={handlerestar} className='btn btn-outline-dark '>-</button>
+                              <span className='mx-2'> {counter} </span>
+                          <button onClick={handlesumar} className='btn btn-dark '>+</button>
+                          <Button variant="dark m-2 start-50">Agregar al Carrito</Button>
+                 </Card.Body>
+              </Card>
         </div>
     )
 
